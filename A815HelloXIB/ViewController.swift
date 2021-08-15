3903d88337aecebbacb315321cab0376ee6d90e0
Danny Shen <shenfive@hotmail.com>
2021-08-15 09:48:52 +0800

    Initial Commit

---
+++ A815HelloXIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A815HelloXIB
+//
+//  Created by 申潤五 on 2021/8/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
