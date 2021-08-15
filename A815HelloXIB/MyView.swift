693653cdf277eb7de825b7c04ed4d4d1fa148796
Danny Shen <shenfive@hotmail.com>
2021-08-15 10:28:11 +0800

    clicked

--- A815HelloXIB/MyView.swift
+++ A815HelloXIB/MyView.swift
@@ -9,26 +9,33 @@ import UIKit
 
 class MyView: UIView {
     var view:UIView!
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            setup()
-        }
-        required init?(coder aDecoder: NSCoder) { //一定要寫的建構器
-            super.init(coder: aDecoder)
-            setup()
-        }
-        func setup() {
-            view = loadViewFromNib()
-            view.frame = bounds
-            view.autoresizingMask = [ UIView.AutoresizingMask.flexibleWidth,
-                UIView.AutoresizingMask.flexibleHeight ]
-            addSubview(view)
-        }
-        
-        func loadViewFromNib() -> UIView {
-            let nib = UINib(nibName: "MyView", bundle: nil )
-            let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
-            return view
-        }
-
+    
+    var buttonClicked:(()->())? = nil
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) { //一定要寫的建構器
+        super.init(coder: aDecoder)
+        setup()
+    }
+    func setup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [ UIView.AutoresizingMask.flexibleWidth,
+                                  UIView.AutoresizingMask.flexibleHeight ]
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let nib = UINib(nibName: "MyView", bundle: nil )
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
+    
+    @IBAction func submit(_ sender: Any) {
+        buttonClicked?()
+    }
 }
--- A815HelloXIB/ViewController.swift
+++ A815HelloXIB/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theMyView: MyView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        theMyView.view.backgroundColor = UIColor.red
+        
+        theMyView.buttonClicked = {
+            print("Hello Xib")
+        }
     }
+
 
 
 }
